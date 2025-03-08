5f197c9dbf80d9587b151cce28b397b2c7613231
Aydın Sarıcan <https://github.com/aydinsarican>
2025-03-08 14:11:06 +0300

    update min ios version

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "SwiftUICoreKit",
     platforms: [
-        .iOS(.v16),
+        .iOS(.v17),
         .macOS(.v15)
     ],
     products: [
